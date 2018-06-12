f6b03aea9f4d8691de4c547db3081e526094962b
xiang.hu <hu.xiang@chinaredstar.com>
2018-06-12 19:57:25 +0800

    代码

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
-import mysql from 'mysql';
-import { mysqlConfig } from './config';
+import bodyParser from 'body-parser';
+import express from 'express';
 
-const pool = mysql.createPool(Object.assign(mysqlConfig, {
-  connectionLimit: 100,
-}));
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-const sql = 'select * from project';
-pool.query(sql, (error: any, result: any) => {
-  if (error) { console.log(error); } else if (result) { console.log(result); }
-});
+const port = 3000;
+// 监听端口
+app.listen(port);
+console.log(`[INFO] ---- EXPRESS STARTED SUCCESSFULL AND LISTEN PORT IN ${port}`);
+// import mysql from 'mysql';
+// import { mysqlConfig } from './config';
+
+// const pool = mysql.createPool(Object.assign(mysqlConfig, {
+//   connectionLimit: 100,
+// }));
+
+// const sql = 'select * from project';
+// pool.query(sql, (error: any, result: any) => {
+//   if (error) { console.log(error); } else if (result) { console.log(result); }
+// });
