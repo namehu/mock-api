9b5ec428daebea4fbc5fb21c62a9a21d11251784
胡祥 (互联网_技术研发中心) <xiang.hu@group.redstar.corp>
2018-09-29 16:40:14 +0800

    更改信息

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const mysqlConfig = {
-  host: '47.98.194.207',
-  user: 'easyMock',
-  password: 'D2Hva2e0qyYUw2To',
-  database: 'easy-mock',
-  port: '3306',
+  host: '',
+  user: '',
+  password: '',
+  database: '',
+  port: '',
 };
